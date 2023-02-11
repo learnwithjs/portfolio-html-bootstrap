b9ee6c575a59f29b3df0e70ff03d58305a265796
learnwithjs <jitendrasuthar995@gmail.com>
2023-02-12 00:54:56 +0530

    completed footer section

--- js/main.js
+++ js/main.js
@@ -38,5 +38,27 @@ $(document).ready(function(){
             }
         }
     })
+
+    // sticky navigation menu
+
+    let nav_offset_top = $('.header_area').height() + 50;
+    console.log("nav height is == ", nav_offset_top)
+
+    function navbarFixed(){
+        console.log("navbar function called")
+        if($('.header_area').length){
+            $(window).scroll(function(){
+                let scroll = $(window).scrollTop();
+                // console.log("scroll number is == ", scroll);
+
+                if(scroll >= nav_offset_top){
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                }else{
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+    navbarFixed();    
 })
 
