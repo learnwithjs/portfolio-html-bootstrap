2a1de2b2fd765d3c6b7f659ab083eee6ca937afb
learnwithjs <jitendrasuthar995@gmail.com>
2023-02-11 21:35:09 +0530

    complete what client say about me section

--- js/main.js
+++ js/main.js
@@ -22,5 +22,21 @@ $(document).ready(function(){
         type:'image',
         gallery: {enabled: true}
     })
+
+
+    // owl carousel
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        dots:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            544:{
+                items:2
+            }
+        }
+    })
 })
 
